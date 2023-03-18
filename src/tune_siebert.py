c0a6c4cb20ee346df0153c0d895a4932c37da0ca
John Zhang <johnz.zhang@mail.utoronto.ca>
2023-03-18 18:13:12 -0400

    tune_siebert changes

--- src/tune_siebert.py
+++ src/tune_siebert.py
@@ -1,5 +1,8 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, AdamW
 import torch
+from load_dataset import Hotel_Dataset
+
+# Tune siebert on hotel sentiment data
 
 # Load the pre-trained model
 model_name = "siebert/sentiment-roberta-large-english"
@@ -8,4 +11,11 @@ model = AutoModelForSequenceClassification.from_pretrained(model_name, num_label
 # Load the tokenizer
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
+# Tokenize hotel review dataset
+path_to_dir = "./data"
+dataset = Hotel_Dataset(path_to_dir, transform=None, target_transform=None)
+
+# 
+encoded_reviews = tokenizer(dataset, padding=True, truncation=True, max_length=512, return_tensors='pt')
+labels = torch.tensor(dataset)
 
